3eab0255837535a000512e162508c2a4dde4e008
Sanjeet Sidhu <Sanjeetu3@gmail.com>
2022-08-06 02:10:32 -0700

    moved functions out, fixed up some names for clarity, removed excess boards in loops, generating those in optional params now.

---
+++ src/utilities/game logic/board/printBoards.ts
@@ -0,0 +1,18 @@
+import { Board } from "../../../types/GameTypes";
+import print from "../../print.js";
+import prettyPrintBoard from "./prettyPrintBoard.js";
+
+export default function printBoards(
+  [...boards]: [Board, Board, Board],
+  debug = false
+) {
+  if (debug) {
+    print("\nYour ENEMY'S is:");
+    prettyPrintBoard(boards[2]); //enemyBoard
+  } else {
+    print("\nYour guess board");
+    prettyPrintBoard(boards[0]); //guessingBoard
+  }
+  print("\nYour board is:");
+  prettyPrintBoard(boards[1]); //playerBoard
+}
--- src/utilities/game logic/gameLoop.ts
+++ src/utilities/game logic/gameLoop.ts
@@ -5,32 +5,35 @@ import createEmptyBoard from "./board/createEmptyBoard.js";
 import fillTileWithHitMarker from "./board/fileTileWithHitMarker.js";
 import mapSunkShips from "./board/mapSunkShips.js";
 import populateBoard from "./board/populateBoard.js";
-import prettyPrintBoard from "./board/prettyPrintBoard.js";
 import tallySunken from "./board/tallySunken.js";
 import state from "../game state/gameState.js";
 import promptPlayersInputs from "./promptPlayerInputs.js";
+import printBoards from "./board/printBoards.js";
+
+console.log(printBoards);
 
 export default async function gameLoop(
-  plBoard: void | Board,
-  enBoard: void | Board,
-  guessBoard: void | Board
+  playerBoard: Board = populateBoard(
+    createEmptyBoard(),
+    state.updatePositions,
+    "player"
+  ),
+  enemyBoard: Board = populateBoard(
+    createEmptyBoard(),
+    state.updatePositions,
+    "enemy"
+  ),
+  guessBoard: Board = createEmptyBoard()
 ) {
   console.clear();
-  const playerBoard =
-    plBoard ||
-    populateBoard(createEmptyBoard(), state.updatePositions, "player");
-  const enemyBoard =
-    enBoard ||
-    populateBoard(createEmptyBoard(), state.updatePositions, "enemy");
-  const guessingBoard = guessBoard || createEmptyBoard();
-
   const playersTurn = state.get().isPlayersTurn;
   const evaluationBoard = playersTurn ? enemyBoard : playerBoard;
   const turnOpponent = playersTurn ? "enemy" : "player";
   const turnPlayer = playersTurn ? "player" : "enemy";
   const gameRenderMS = 2250;
 
-  //By updating the sunkenShips on the start of each gameLoop, the printed board will not reflect changes untill its time for the player's next turn.
+  // By updating the sunkenShips on the start of each gameLoop,
+  // The printed board will not reflect changes untill its time for the player's next turn.
   const sunkenShips = mapSunkShips(
     state.get().positions[turnOpponent],
     state.get().shipsHit[turnOpponent]
@@ -43,21 +46,12 @@ export default async function gameLoop(
     state.updateGameHasEnded(true);
     return true;
   }
-  function printBoards(debug = false) {
-    if (debug) {
-      print("\nYour ENEMY'S is:");
-      prettyPrintBoard(enemyBoard);
-    } else {
-      print("\nYour guess board");
-      prettyPrintBoard(guessingBoard);
-    }
-    print("\nYour board is:");
-    prettyPrintBoard(playerBoard);
-  }
-  print(playersTurn ? "Player's turn" : "AI turn");
+
   // Board state shown while player plays their turn move.
   // this function call will not print on enemy's turn
-  printBoards(true);
+  print(playersTurn ? "Player's turn" : "Enemy's turn");
+  // [ [ ---> NOTE <--- ] ] : printBoards(a, b = true) enables debugging
+  printBoards([guessBoard as Board, playerBoard as Board, enemyBoard as Board]);
 
   //If it is the players turn, we are evaluating the shots fired on the rival board. and updating that turn's (player/ai) board
   const initialCoords: XYCoords = { x: 0, y: 0 };
@@ -67,7 +61,6 @@ export default async function gameLoop(
     if (typeof result === "object") {
       (y = result.y), (x = result.x);
     } else if (result === "end") {
-      //end game logic here?
       state.updateGameHasEnded(true);
       print("\nThanks for playing");
       return true;
@@ -76,10 +69,10 @@ export default async function gameLoop(
     y = Math.floor(Math.random() * evaluationBoard.length);
     x = Math.floor(Math.random() * evaluationBoard[y].length);
   }
-  // UTILS ===================================
+  // TILE FILL UTILS ===================================
   const red = (tile: string) => "\x1b[41m" + tile + "\x1b[0m";
   const yellow = (tile: string) => "\x1b[43m" + tile + "\x1b[0m";
-  // UTILS ===================================
+  // TILE FILL UTILS ===================================
 
   const { hit, tile } = checkForHit({ y, x }, evaluationBoard);
   const repeatShot = state.checkIfPreviouslyHitTile({ x, y }, turnPlayer);
@@ -119,14 +112,13 @@ export default async function gameLoop(
     ? fillTileWithHitMarker({ x, y }, fill, playerBoard)
     : playerBoard;
   const newGuessingBoard = playersTurn
-    ? fillTileWithHitMarker({ x, y }, fill, guessingBoard)
-    : guessingBoard;
+    ? fillTileWithHitMarker({ x, y }, fill, guessBoard)
+    : guessBoard;
 
   //printBoards() here because without it, the function moves out to the outter body before the setTimeout executes.
   // board printed right after either player or the AI finish their moves and persists during the timeout for the new board.
   // After the turnPlayer finishes their move, this function call is the one that persists on screen
-  printBoards();
-
+  printBoards([guessBoard as Board, playerBoard as Board, enemyBoard as Board]);
   //by enveloping gameLoop in a setTimeout to delay by `n` seconds, the promise guarantees it will not fire untill completion of the timeout AND we will not prematurely return a non promise based value, which will continue thread of execturion after first return
   return new Promise((res) => {
     setTimeout(async () => {
